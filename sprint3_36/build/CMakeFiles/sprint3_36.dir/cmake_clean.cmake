aefd915c35de0463da91ae0ca78fef2cb932d639
Darkmonkeyz <darkmonkeyzgaming@gmail.com>
2024-10-06 13:12:24 +0000

    Sprint336

---
+++ sprint3_36/build/CMakeFiles/sprint3_36.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sprint3_36.dir/src/sprint3_36.cpp.o"
+  "CMakeFiles/sprint3_36.dir/src/sprint3_36.cpp.o.d"
+  "sprint3_36"
+  "sprint3_36.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sprint3_36.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
